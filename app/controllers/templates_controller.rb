39d3f02527ab5700e84631a5ad9826068db9f439
Katarina Tuttle <katarina.tuttle@alphamediausa.com>
2016-10-20 15:09:34 -0700

    allow admin to update saved templates

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,3 +1,40 @@
 class TemplatesController < ApplicationController
-  
+  def show
+    # @category = Category.find(params[:category_id])
+    @template = Template.find(params[:id])
+  end
+
+  def new
+    @template = Template.new
+    render :new
+  end
+
+  def create
+    @template = Template.new(template_params)
+    if @template.save
+      flash[:notice] = "Template successfully added!"
+      redirect_to template_path(@template)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @template = Template.find(params[:id])
+  end
+
+  def update
+    @template = Template.find(params[:id])
+    if @template.update(template_params)
+      flash[:notice] = "Template successfully updated!"
+      redirect_to template_path(@template)
+    else
+      render :edit
+    end
+  end
+
+private
+  def template_params
+    params.require(:template).permit(:title, :description)
+  end
 end
