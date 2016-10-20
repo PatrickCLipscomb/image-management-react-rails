6238a69f6f523bff676ec2c09d7d3dd0c445c0d9
Patrick Lipscomb <patrick.lipscomb@alphamediausa.com>
2016-10-20 14:44:52 -0700

    add a table structure to the admin page and create a delete button

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,3 +1,11 @@
 class TemplatesController < ApplicationController
-  
+  def destroy
+    template = Template.find(params[:id])
+    if template.delete
+      flash[:notice] = template.title.capitalize + " Template Deleted"
+      redirect_to admin_path
+    else
+      flash[:alert] = "Template Delete Failed"
+    end
+  end
 end
