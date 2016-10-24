48d3c2dcffe4b53bc87ed646f0954a6b4fe89d1a
Patrick Lipscomb <patrick.lipscomb@alphamediausa.com>
2016-10-24 15:15:40 -0700

    it will add immensely dirty code in order to get a working filter, should be refactored

--- app/assets/javascripts/components/templates.js.jsx
+++ app/assets/javascripts/components/templates.js.jsx
@@ -3,33 +3,72 @@
 class Templates extends BaseComponent {
     constructor(props) {
         super();
-        this._bind('addTemplate', 'deleteTemplate', 'handleEditTemplate');
+        this._bind('addTemplate', 'deleteTemplate', 'handleEditTemplate', 'handleCatCycle');
         this.state = {
             categories: props.data[0],
-            templates: props.data[1]
+            templates: props.data[1],
+            filteredTemplates: props.data[1]
         };
     }
 
     addTemplate(template) {
-
         var templates = React.addons.update(this.state.templates, {$push: [template]});
         this.setState({ templates: templates });
     }
 
     deleteTemplate(template) {
+        if (this.state.filteredTemplates.includes(template)) {
+          var filtIndex = this.state.filteredTemplates.indexOf(template)
+          var templatesFiltered = React.addons.update(this.state.filteredTemplates, {$splice: [[index, 1]]});
+          this.setState({filteredTemplates: templatesFiltered})
+        }
         var index = this.state.templates.indexOf(template);
         var templates = React.addons.update(this.state.templates, {$splice: [[index, 1]]});
         this.setState({ templates: templates });
     }
 
     handleEditTemplate(template, data) {
+        if (this.state.filteredTemplates.includes(template)) {
+          var filtIndex = this.state.filteredTemplates.indexOf(template)
+          var templatesFiltered = React.addons.update(this.state.filteredTemplates, {$splice: [[index, 1, data]]});
+          this.setState({filteredTemplates: templatesFiltered})
+        }
         var index = this.state.templates.indexOf(template);
         var templates = React.addons.update(this.state.templates, {$splice: [[index, 1, data]]});
         this.setState({ templates: templates} );
     }
 
+
+    handleCatCycle(event) {
+      var selected = event.currentTarget.value
+      var templates = this.state.templates
+      var categories = this.state.categories
+      var filteredCategories = []
+      var catName = function(category_id) {
+        var name;
+        categories.forEach(function(cat) {
+          if (cat.id === category_id) {
+            name = cat.name
+          }
+        })
+        return name
+      }
+      if (selected === 'All') {
+        templates.forEach(function(template) {
+          filteredCategories.push(template)
+        })
+      } else {
+        templates.forEach(function(template) {
+          if (catName(template.category_id) === selected) {
+            filteredCategories.push(template)
+          }
+        })
+      }
+      this.setState({filteredTemplates: filteredCategories})
+    }
+
     render() {
-        var templates = this.state.templates.map((template, index) => {
+        var templates = this.state.filteredTemplates.map((template, index) => {
             return <Template key={template.id} template={template} categories={this.state.categories}
                            handleDeleteTemplate={this.deleteTemplate} handleEditTemplate={this.handleEditTemplate} />
         });
@@ -44,7 +83,14 @@ class Templates extends BaseComponent {
                         <tr>
                             <th> Title </th>
                             <th> Description </th>
-                            <th> Category </th>
+                            <th> Category <br />
+                              <select value={this.state.catCycle} onChange={this.handleCatCycle}>
+                                <option value="All">All</option>
+                                <option value="Advertising">Advertising</option>
+                                <option value="Social Media">Social Media</option>
+                                <option value="Website Graphics">Website Graphics</option>
+                              </select>
+                            </th>
                             <th> Actions </th>
                         </tr>
                     </thead>
