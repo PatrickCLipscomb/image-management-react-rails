e46a3ed11e65ca38d258b193d87f0eaba46cddfb
Katarina Tuttle <katarina.tuttle@alphamediausa.com>
2016-10-28 15:46:47 -0700

    edit seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 User.create!(email: "admin@admin.com", username: "AlphaAdmin", password: "password")
 Category.all.each {|a| a.destroy }
-Category.create(name: "Social Media", description: "To be used on social media platforms")
-Category.create(name: "Advertising", description: "To be used to promote Alpha Media")
-Category.create(name: "Website Graphics", description: "To be used for website graphics")
+Category.create!(name: "Social Media", description: "To be used on social media platforms")
+Category.create!(name: "Advertising", description: "To be used to promote Alpha Media")
+Category.create!(name: "Website Graphics", description: "To be used for website graphics")
 Template.all.each {|a| a.destroy }
 30.times do
-  Template.create(title: Faker::Hacker.adjective, description: Faker::Hacker.say_something_smart, category_id: Category.all.first.id + rand(3))
+  Template.create!(title: Faker::Hacker.adjective, description: Faker::Hacker.say_something_smart, category_id: Category.all.first.id + rand(3))
 end
