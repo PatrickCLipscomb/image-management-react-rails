b1c91199cf01dc417a0c8c75bc1b31b008899fe2
Katarina Tuttle <katarina.tuttle@alphamediausa.com>
2016-10-28 14:55:35 -0700

    edit route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
     root to: "home#index"
 
-  get 'admin', to: "categories#admin_index"
-  get 'react', to: "categories#react_index"
+  get 'admin' => "categories#admin_index"
+  get 'react' => "categories#react_index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
