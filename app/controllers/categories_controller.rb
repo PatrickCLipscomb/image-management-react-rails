3e7931f08f71545f8c38aa8cd9e2b0d974b1a6a2
Patrick Lipscomb <patrick.lipscomb@alphamediausa.com>
2016-11-08 16:17:35 -0800

    change how categories are seeded, now seeded in the controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
   def index
     @categories = Category.all
   end
@@ -7,6 +7,11 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
   def react_index
+    if Category.all.length == 0
+      Category.create!(name: "Social Media", description: "To be used on social media platforms", image: "/assets/Social_Image_Icon.svg")
+      Category.create!(name: "Web Ad", description: "To be used to promote Alpha Media", image: "/assets/Web_Ad_Icon.svg")
+      Category.create!(name: "Web Image", description: "To be used for website graphics", image: "/assets/Web_Image_Icon.svg")
+    end
     @categories = []
     ['Social Media', 'Web Ad', 'Web Image'].each do |categoryName|
       @categories.push(Category.where(name: categoryName))
