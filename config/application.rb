4ba5120be91b1a95f918af8a4942f93423a70a88
Katarina Tuttle <katarina.tuttle@alphamediausa.com>
2016-11-11 11:52:20 -0800

    add image files extensions

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module DigitalResources
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     config.active_record.raise_in_transactional_callbacks = true
     config.react.addons = true
   end
