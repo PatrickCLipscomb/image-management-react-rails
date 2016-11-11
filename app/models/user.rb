8863991cd15225b02abfe5601a69525af7d6fa10
Katarina Tuttle <katarina.tuttle@alphamediausa.com>
2016-11-11 14:51:37 -0800

    unable registering for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :authentication_keys => [:username]
-  devise :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :recoverable, :rememberable, :trackable, :validatable
 end
