156987aec4c2a882024bfbb86575b9af525d26cd
Katarina Tuttle <katarina.tuttle@alphamediausa.com>
2016-11-15 08:56:43 -0800

    add passing spec

--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Template, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Template do
+  it {should belong_to :category}
+  it {should validate_presence_of :image }
 end
